57c2708058c304f2f8290b371f9565f1bc90172b
giannos <john.char.dev@gmail.com>
2022-12-09 14:09:41 +0200

    landing page containing social links, fix issue with scrolling of should be full height container => absolute leaf to fixed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 import Head from "next/head";
 
+const links = [
+    {
+        name: 'Instagram',
+        link: 'https://www.instagram.com/floga.decor/',
+        icon: 'instagram',
+    },
+    {
+        name: 'Facebook',
+        link: 'https://www.facebook.com/flogadecor/',
+        icon: 'facebook',
+    },
+]
+
 const Home = () => {
     return (
         <>
@@ -7,17 +20,22 @@ const Home = () => {
                 <title>Coming soon - Floga Decor</title>
                 <link rel="icon" href="/leaf.webp" />
             </Head>
-            <main style={{height: '60vh'}} className='flex flex-col justify-end bg-[#f8f2e8]'>
-                <img src='/leaf.webp' style={{position: 'absolute', right: -200, top: -200}}/>
-                <div className='text-center'>
-
-                    <h1 className='text-6xl md:text-7xl lg:text-8xl mb-6' style={{fontFamily: 'Dancing Script', color: '#69524b'}}>Coming Soon</h1>
-                    <h2 className='text-4xl md:text-5xl lg:text-6xl' style={{fontFamily: 'Oswald', color: '#69524b'}}>STAY TUNED</h2>
-                </div>
-            </main>
-            <footer style={{height: '40vh'}} className='grid place-items-center bg-[#f8f2e8]'>
-                <img src='/logo2.png' alt='logo' className='w-36 md:w-44 lg:w-56 mx-auto' />
-            </footer>
+                <main className='flex items-center flex-col justify-center' style={{ height: "100vh", width: "100vw", background: '#f8f2e8'}}>
+                    <img alt='' src='/leaf.webp' style={{position: 'fixed', right: -200, top: -200}}/>
+                    <img src='/logo2.png' alt='logo' className='w-36 md:w-44 lg:w-56 mx-auto mb-24' />
+                    <div className='text-center mb-10'>
+                        <h1 className='text-4xl md:text-5xl lg:text-6xl mb-6' style={{fontFamily: 'Dancing Script', color: '#69524b'}}>Welcome &<br/>thanks for scanning</h1>
+                        <h2 className='text-2xl md:text-2xl lg:text-4xl' style={{fontFamily: 'Oswald', color: '#69524b'}}>FOLLOW US ON SOCIAL MEDIA</h2>
+                    </div>
+                    <ul className='flex flex-col gap-4'>
+                        {links.map(({name, link, icon}) => (
+                            <li key={name} className='flex items-center gap-2'>
+                                <a href={link} target
+                                ='_blank' rel='noreferrer' className='text-2xl md:text-3xl lg:text-5xl' style={{fontFamily: 'Oswald', color: '#69524b'}}>{name}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </main>
 
         </>
     );
